aef536a0d1054faf891de67f083973c5fcc93cad
Randy's Mac <randydavidl78@gmail.com>
2018-04-07 14:54:21 -0400

    Add methods to check for winner to GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -1,8 +1,81 @@
+import java.sql.SQLFeatureNotSupportedException;
+
 /**
  * GameLogic
  * Class for testing board to check for wins 
+ * @author Randy Layne
  */
 public class GameLogic {
+  /** checks if player won */
+  public static boolean checkWin(Board board, int currentPlayer) {
+    boolean won = false;
+    
+    won = checkRows(board, currentPlayer) || won;
+    won = checkColumns(board, currentPlayer) || won;
+    won = checkDecreaseDiagonal(board, currentPlayer) || won;
+    won = checkIncreaseDiagonal(board, currentPlayer) || won;
 
+    return won;
+  }
+
+  /** checks if player won with three in a row*/
+  private static boolean checkRows(Board board, int currentPlayer) {
+    boolean won = false;
+    // loop through each row and check for winner
+    for  (int i = 0; i < 3; i++) {
+      won = checkRow(board, currentPlayer, i) || won; 
+    }
+    return won;
+  }
+
+  /** check a single row for winner */
+  private static boolean checkRow(Board board, int currentPlayer, int row) {
+    for (int i = 0; i < 3; i++) {
+      if (board.getOwner(row, i) != currentPlayer)
+        return false;
+    }
+    return true;
+  }
+
+  /** checks if player won with three in a row*/
+  private static boolean checkColumns(Board board, int currentPlayer) {
+    boolean won = false;
+    // loop through each column and check for winner
+    for  (int i = 0; i < 3; i++) {
+      won = checkColumn(board, currentPlayer, i) || won; 
+    }
+    return won;
+  }
   
+  /** check a single column for winner */
+  private static boolean checkColumn(Board board, int currentPlayer, int col) {
+    for (int i = 0; i < 3; i++) {
+      if (board.getOwner(i, col) != currentPlayer)
+        return false;
+    }
+    return true;
+  }
+
+  /** check diagonal with negative slope for win */
+  private static boolean checkDecreaseDiagonal(Board board, int currentPlayer) {
+    boolean won = true;
+    
+    for (int i = 0; i < 3; i++) {
+      if (board.getOwner(i, i) != currentPlayer)
+        return false;
+    }
+    return won;
+  }
+
+  /** check diagonal with positive slope for win */
+  private static boolean checkIncreaseDiagonal(Board board, int currentPlayer) {
+    boolean won = true;
+    
+    for (int i = 0; i < 3; i++) {
+      if (board.getOwner(i, 2 - i) != currentPlayer)
+        return false;
+    }
+    return won;
+  }
+
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -12,16 +12,23 @@ public class TicTacToe {
     Players players = new Players(new HumanPlayer(), new ComputerPlayer());
 
     Scanner input = new Scanner(System.in);
+    // Display initial game board
+    GameConsole.clearScreen();
+    GameConsole.printBoard(board);
 
     // Loop taking turns until user quits
     boolean quit = false;
     do {
+      // Take turn and determine if user quit
+      quit = GameConsole.takeTurn(board, players, input);
+
       // Display the game board in current state
       GameConsole.clearScreen();
       GameConsole.printBoard(board);
 
-      // Take turn and determine if user quit
-      quit = GameConsole.takeTurn(board, players, input);
+      // If player won, exit;
+      if (GameLogic.checkWin(board, players.currentPlayer())) 
+        quit = true;
 
       // Switch player turns
       players.flip();
