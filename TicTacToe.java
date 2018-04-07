9e0b3dbe2929097afcc40c344e55346073dd1094
Randy's Mac <randydavidl78@gmail.com>
2018-04-07 13:17:36 -0400

    added clearScreen method to GameConsole.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,21 +7,28 @@ import java.util.Scanner;
  */
 public class TicTacToe {
   public static void main(String[] args) {
-    // Initialize board and players
+    // Initialize board and players  
     Board board = new Board();
     Players players = new Players(new HumanPlayer(), new ComputerPlayer());
 
-    boolean quit = false;
-
     Scanner input = new Scanner(System.in);
+
+    // Loop taking turns until user quits
+    boolean quit = false;
     do {
+      // Display the game board in current state
+      GameConsole.clearScreen();
       GameConsole.printBoard(board);
+
+      // Take turn and determine if user quit
       quit = GameConsole.takeTurn(board, players, input);
 
+      // Switch player turns
       players.flip();
 
     } while (!quit);
 
+    // Quit the game
     System.out.println("Goodbye!");
 
   }
