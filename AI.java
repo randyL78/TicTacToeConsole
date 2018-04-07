7ac19bb9d95f2ff1d45feac54bfc045e43f7c5f7
Randy's Mac <randydavidl78@gmail.com>
2018-04-07 15:19:33 -0400

    implemented easyTurn in AI.java

--- AI.java
+++ AI.java
@@ -5,31 +5,46 @@
  */
 public class AI {
   public static final int MAX_DIFFICULTY = 2;
-  public static void takeTurn(Board board, int difficulty) {
+  public static void takeTurn(Board board, Players players) {
+    // Get the difficulty level of current computer player
+    int difficulty = ((ComputerPlayer) (players.players[players.currentPlayer()])).getDifficulty();
+
     switch (difficulty) {
       case 0:
-        easyTurn(board);
+        easyTurn(board, players.currentPlayer());
         break;
       case 1:
-        mediumTurn(board);
+        mediumTurn(board, players.currentPlayer());
         break;
       case 2:
-        hardTurn(board);
+        hardTurn(board, players.currentPlayer());
         break;
       default:
         throw new IllegalArgumentException("Difficulty must be in the range of 0 - " + MAX_DIFFICULTY);
     }
   }
 
-  private static void easyTurn(Board board) {
-    // Make a turn based on a random index of available Squares
+  private static void easyTurn(Board board, int player) {
+    // loop until random square is not taken 
+    boolean taken = true;
+    do {
+      // get random row and column number
+      int row = (int) (Math.random() * 3);
+      int col = (int) (Math.random() * 3);
+      if (board.isEmptyAt(row, col)) {
+        board.setOwner(player, row, col);
+        taken = false;
+      }
+
+    } while (taken);
+
   }
   
-  private static void mediumTurn(Board board) {
+  private static void mediumTurn(Board board, int player) {
     // Make a turn based on a basic algorithm
   }
 
-  private static void hardTurn(Board board) {
-    // Make a turn based on a random index of available Squares
+  private static void hardTurn(Board board, int player) {
+    // Make a turn based on a more strategic algorithm
   }
 }
