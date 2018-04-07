2a78e646a3fc8e61560ecad6e15537f4a041eb04
Randy's Mac <randydavidl78@gmail.com>
2018-04-07 10:29:55 -0400

    Created stubs for AI, File Actions, Game Console, and Game Logic

---
+++ FileActions.java
@@ -0,0 +1,9 @@
+/**
+ * FileActions
+ * Performs saving and loading of playerss to a txt file
+ * @author Randy Layne
+ */
+public class FileActions {
+
+  
+}
---
+++ GameLogic.java
@@ -0,0 +1,8 @@
+/**
+ * GameLogic
+ * Class for testing board to check for wins 
+ */
+public class GameLogic {
+
+  
+}
