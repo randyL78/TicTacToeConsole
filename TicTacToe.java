c7b328dbb40eb31e74609c09a3b5b941babe1205
Randy's Mac <randydavidl78@gmail.com>
2018-04-25 22:48:15 -0400

    added difficulty selector to intro screen

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
@@ -23,6 +27,7 @@ import javafx.stage.Stage;
  * @Randy Layne
  */
 public class TicTacToe extends Application {
+  public Players players;
   /** Main entry point into application */
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -53,8 +58,40 @@ public class TicTacToe extends Application {
 
     // handle Play button click
     btnPlay.setOnAction( e -> {
-      System.out.println(player1Pane.getName());
-      System.out.println(player2Pane.getName());
+      // Initialize Players
+      players = new Players();
+
+      // Add Players based on if they are computer or human
+      if (player1Pane.getName().equalsIgnoreCase("computer")) {
+        // Create a computer player and add to players
+        players.replace(new ComputerPlayer(player1Pane.getDifficulty()), 0);
+      } else {
+        try {
+          // Load player 1 into Players
+          players.replace(FileActions.loadPlayer(new HumanPlayer(player1Pane.getName())), 0);
+        } catch (IOException ex) {
+          // If there is an error, just create a new player based on field name
+          players.replace(new HumanPlayer(player1Pane.getName()), 0);
+        }
+      }
+
+
+      if (player2Pane.getName().equalsIgnoreCase("computer")) {
+        // Create a computer player and add to players
+        players.replace(new ComputerPlayer(player2Pane.getDifficulty()), 1);
+      } else {
+        try {
+          // Load player 1 into Players
+          players.replace(FileActions.loadPlayer(new HumanPlayer(player1Pane.getName())), 1);
+        } catch (IOException ex) {
+          // If there is an error, just create a new player based on field name
+          players.replace(new HumanPlayer(player1Pane.getName()), 1);
+        }
+      }
+
+      System.out.println(players.players[0]);
+      System.out.println(players.players[1]);
+
       primaryStage.show();
       introStage.close();
     });
@@ -74,7 +111,7 @@ public class TicTacToe extends Application {
     pnIntro.setBottom(hBoxForIntroButtons);
 
     // Create scene and display stage
-    Scene scnIntro = new Scene(pnIntro, 300, 400);
+    Scene scnIntro = new Scene(pnIntro, 300, 500);
     introStage.setTitle("Welcome to Tic Tac Toe");
     introStage.setScene(scnIntro);
     introStage.show();
@@ -105,6 +142,9 @@ class PlayerSelectPane extends VBox {
   private int number;
   private CheckBox chkHuman;
   private TextField txtPlayer;
+  private Text difficultyText;
+  private ComboBox cbDifficulty;
+
 
   public PlayerSelectPane() {
     // pane styles
@@ -123,23 +163,37 @@ class PlayerSelectPane extends VBox {
     // player name selection
     txtPlayer = new TextField("Name");
 
+    // computer difficulty selector
+    difficultyText = new Text("Difficulty Level");
+    cbDifficulty = new ComboBox<>(FXCollections.observableArrayList(new String[] {"Easy", "Medium", "Hard"}));
+    // set default difficulty to medium
+    cbDifficulty.getSelectionModel().select(1);
+
+
     // default first player to human
     if (number == 1) {
       chkHuman.setSelected(true);
+      cbDifficulty.setDisable(true);
+      difficultyText.setFill(Color.GRAY);
     } else {
       // other player defaults to computer, and name field is disabled
       txtPlayer.setDisable(true);
     }
 
     // add fields to pane
-    getChildren().addAll(titleText, chkHuman, txtPlayer);
+    getChildren().addAll(titleText, chkHuman, txtPlayer, difficultyText, cbDifficulty);
 
     // handle checkbox select event
     chkHuman.setOnAction(e -> {
-      if (chkHuman.isSelected()) 
+      if (chkHuman.isSelected()) {
         txtPlayer.setDisable(false);
-      else 
+        cbDifficulty.setDisable(true);
+        difficultyText.setFill(Color.GRAY);
+      } else { 
         txtPlayer.setDisable(true);
+        cbDifficulty.setDisable(false);
+        difficultyText.setFill(Color.BLACK);
+      }
     });
 
     // handle text field event
@@ -163,5 +217,16 @@ class PlayerSelectPane extends VBox {
     else 
       // if user has entered a value for name, return it
       return txtPlayer.getText();
+  }
+
+  /**
+   * @return difficulty of computer player or -1 if human
+   */
+  public int getDifficulty() {
+    // see if checkbox is checked
+    if (chkHuman.isSelected()) 
+      return -1;
+    else 
+      return cbDifficulty.getSelectionModel().getSelectedIndex();
   }
 }
